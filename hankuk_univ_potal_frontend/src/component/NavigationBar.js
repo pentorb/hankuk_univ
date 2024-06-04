e08ef4d506df3b36784f3a6cf8379ff487d803c0
Hyun <83358018+hyunmne@users.noreply.github.com>
2024-06-04 16:07:01 +0900

    design: nav font 수정

--- hankuk_univ_potal_frontend/src/component/NavigationBar.js
+++ hankuk_univ_potal_frontend/src/component/NavigationBar.js
@@ -5,7 +5,7 @@ export default function NavigationBar() {
     <div style={{ width: '100%', height: '85px', display: 'flex', margin: '0 auto' }}>
       <img src="/images/logo2.png" style={{ width: '50px', height: '50px', margin: '15px 10px 15px 20px' }} />
       <span style={{ fontSize: "24px", fontFamily: "ChosunLo", color: "var(--maincolor)", margin: '23px 5px 23px 0    ' }}>한국대학교 </span>
-      <span style={{ fontSize: "22px", fontWeight: '600', color: "var(--maincolor)", margin: '23px 0' }}>/ 종합학사포탈</span>
+      <span style={{ fontSize: "22px", fontFamily: 'Pretendard-Regular', fontWeight: '600', color: "var(--maincolor)", margin: '23px 0' }}>/ 종합학사포탈</span>
     </div>
   );
 }
