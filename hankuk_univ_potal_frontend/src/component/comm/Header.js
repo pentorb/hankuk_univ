73b51ff7d27f30e0469e937eeacf23d0cdebddd8
Youngwoo Choi <allnstar@naver.com>
2024-06-05 12:48:26 +0900

    feat: 헤더에 링크 추가

--- hankuk_univ_potal_frontend/src/component/comm/Header.js
+++ hankuk_univ_potal_frontend/src/component/comm/Header.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div style={{ width: '100%', height: '85px', display: 'flex', margin: '0 auto' }}>
             <img src="/images/logo2.png" style={{ width: '50px', height: '50px', margin: '15px 10px 15px 20px' }} />
-            <span style={{ fontSize: "24px", fontFamily: "ChosunLo", color: "var(--maincolor)", margin: '23px 5px 23px 0    ' }}>한국대학교 </span>
-            <span style={{ fontSize: "22px", fontWeight: '600', color: "var(--maincolor)", margin: '23px 0' }}>/ 종합학사포탈</span>
+            <span style={{ margin: '23px 5px 23px 0' }}><Link to="/" style={{textDecoration:"none", color: "var(--maincolor)", fontSize: "24px", fontFamily: "ChosunLo"}}>한국대학교</Link> </span>
+            <span style={{ margin: '23px 0' }}>/ <Link to="/my-potal" style={{textDecoration:"none", color: "var(--maincolor)", fontSize: "22px", fontWeight: '600'}}>종합학사포탈</Link></span>
         </div>
     )
 }
