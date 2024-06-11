13828003d7a776b1454a3eb640a0da6243729f84
hyunmne <83358018+hyunmne@users.noreply.github.com>
2024-06-12 00:10:14 +0900

    design: main / css

--- hankuk_univ_potal_frontend/src/component/comm/Main.js
+++ hankuk_univ_potal_frontend/src/component/comm/Main.js
@@ -1,76 +1,87 @@
 import Toc from "./Toc";
 import { Link } from 'react-router-dom';
+import '../comm/css/Main.css';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Main = () => {
     return (
-        <div style={{ width: '1300px', height: '1200px', margin: "0 auto", position: 'relative' }}>
-            <div style={{ width: "1300px", height: "532px", margin: "0 auto", position: 'relative', backgroundImage: 'url(/images/background.jpg)' }}>
-                <div style={{ display: 'flex', position: 'relative', zIndex: 2, marginLeft: '20px' }}> {/* 수정된 부분 */}
-                    <span style={{ margin: '10px 5px' }}><img src="/images/logo2.png" alt="" style={{ width: '40px' }} /></span>
-                    <span style={{
-                        fontSize: "24px", fontFamily: "ChosunLo", color: "white", margin: '13px 3px'
-                    }}>
-                        한국대학교
-                    </span >
-                    <span style={{ fontSize: "22px", color: "white", margin: '13px 3px' }}> / 종합학사포탈</span >
-                </div>
-                <div>
-                    <div style={{ bottom: 0, left: 0 }}>
-                        <div style={{ fontSize: '32px', fontWeight: '900', color: 'white', display: 'flex', marginTop: '240px', marginLeft: '60px' }}>
-                            끝없는 도전으로 새로운 길을 개척하는 힘
+        <div>
+            {/* 배경 이미지 */}
+            <div className="main-container">
+                <div className="main-image"></div>
+            </div>
+            
+            {/* 본문 */}
+            <div className="body">
+                <div className="secimage">
+                    {/* 헤더 */}
+                    <div style={{ display: 'flex', position: 'relative', zIndex: 2, marginLeft: '20px' }}>
+                        <span style={{ margin: '10px 5px' }}><img src="/images/logo2.png" alt="" style={{ width: '40px' }} /></span>
+                        <span style={{
+                            fontSize: '24px', fontFamily: 'ChosunLo', color: 'white', margin: '13px 3px'
+                        }}>
+                            한국대학교
+                        </span>
+                        <span style={{ fontSize: '22px', color: 'white', margin: '13px 3px' }}> / 종합학사포탈</span>
+                    </div>
+
+                    {/* 본문 시작 */}
+                    <div >
+                        <div style={{ bottom: 0, left: 0 }}>
+                            <div className="slogan">
+                                끝없는 도전으로 새로운 길을 개척하는 힘
+                            </div>
+                        </div>
+                        <Link to="/login" className="loginBtn">
+                            <div style={{ fontSize: '25px', fontWeight: '700' }}>로그인(Login)</div>
+                        </Link>
+                    </div>
+                    <div className="tocDiv">
+                        <Toc />
+                    </div>
+
+                    <span style={{ display: 'flex' }}>
+                        <div className="calDiv">
+                            <div className="ttt" style={{color:'white', padding:'30px 0px 20px'}}>교내 일정</div>
+                            <div style={{ backgroundColor: 'white', height: 'auto', padding:'20px 20px 10px 20px', margin: '0 50px' }}>
+                                <FullCalendar
+                                    headerToolbar={{
+                                        left: "",
+                                        center: "title",
+                                        right: "",
+                                    }}
+                                    defaultView="dayGridMonth"
+                                    // locale={'ko'}
+                                    // dayCellContent={daycheck}
+                                    plugins={[dayGridPlugin, interactionPlugin]}
+                                    // events={events}
+                                    // eventContent={renderEventContent}
+                                    dayMaxEventRows={true}
+                                    dayMaxEvents={2}
+                                    contentHeight={400}
+                                />
+                            </div>
+                        </div>
+                        <div style={{ margin: '200px 0 0 80px'}}>
+                            <div className="ttt" style={{ margin: '0px 0px 10px 10px'}}>NOTICE</div>
+                            <div style={{ width: '700px', height: '460px', backgroundColor: 'var(--maincolor6)' }}></div>
+                        </div>
+                    </span>
+                    <div>
+                        <div style={{ margin: '50px 0 0 60px', }}>
+                            <div className="ttt" style={{ margin: '10px 0 10px 60px'}}>NEWS</div>
+                            <span style={{ display: 'flex', margin: '0 80px', justifyContent: 'space-around' }}>
+                                <div className="newsBox"></div>
+                                <div className="newsBox"></div>
+                                <div className="newsBox"></div>
+                            </span>
                         </div>
                     </div>
-                    <Link to="/login" style={{
-                        position: 'absolute',
-                        borderRadius: '50px',
-                        backgroundColor: 'var(--subcolor)',
-                        color: 'white',
-                        left: '60px',
-                        bottom: '100px',
-                        width: '450px',
-                        height: '70px',
-                        textDecorationLine: 'none',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                        <div style={{ fontSize: "25px", fontWeight: '700' }}>로그인(Login)</div>
-                    </Link>
                 </div>
-                <div style={{
-                    position: 'absolute',
-                    bottom: '30px',
-                    right: '20px',
-                    width: '100px',
-                    height: '400px'
-                }}>
-                    <Toc />
-                </div>
-                <span style={{ display: 'flex' }}>
-                    <div style={{ width: '450px', height: '450px', borderRadius: '15px 15px 0 0', backgroundColor: "var(--maincolor)", marginTop: "120px", marginLeft: '60px' }}>
-                        <div style={{ fontSize: "32px", color: 'white', fontWeight: 'bolder', padding: '30px 0 20px 0', display: "flex", marginLeft: '60px' }}>교내 일정</div>
-                        <div style={{ backgroundColor: 'white', width: '350px', height: '300px', margin: '0 50px' }}></div>
-                    </div>
-                    <div style={{ margin: '200px 0 0 80px', }}>
-                        <div style={{ fontSize: '32px', fontWeight: "bolder", display: "flex", marginBottom: '10px' }}>NOTICE</div>
-                        <div style={{ width: '640px', height: '310px', backgroundColor: 'var(--maincolor6)' }}></div>
-                    </div>
-                </span>
-                <div>
-                    <div style={{ margin: '20px 0 0 60px', }}>
-                        <div style={{ fontSize: '32px', fontWeight: "bolder", display: "flex", margin: '10px 0 10px 60px' }}>NEWS</div>
-                        <span style={{ display: 'flex', margin: '0 80px' }}>
-                            <div style={{ width: '500px', height: '150px', backgroundColor: 'var(--maincolor6)', borderRadius: '40px 0 40px 0' }}></div>
-                            <div style={{ width: '500px', height: '150px', backgroundColor: 'var(--maincolor6)', marginLeft: '20px', borderRadius: '40px 0 40px 0' }}></div>
-                        </span>
-                    </div>
-                </div>
-                {/* <div style={{ backgroundColor: 'darkgray', width: '100%', height: '150px', margin: '30px 0 0 0' }}>
-
-                </div> */}
-            </div >
-
-        </div >
+            </div>
+        </div>
     );
 }
 
