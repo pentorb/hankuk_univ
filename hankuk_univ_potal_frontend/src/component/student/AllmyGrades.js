719c991d400ec17ab78ca942fdfbfdd71c506db3
Hyun <83358018+hyunmne@users.noreply.github.com>
2024-06-12 12:24:27 +0900

    design: 학점 내역 (강의 리스트) 화면 최종

--- hankuk_univ_potal_frontend/src/component/student/AllmyGrades.js
+++ hankuk_univ_potal_frontend/src/component/student/AllmyGrades.js
@@ -1,11 +1,10 @@
 import Grid from '@mui/material/Grid';
-import { Paper, Typography, Button, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Paper, Typography, Select, MenuItem, FormControl } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Input } from 'reactstrap';
 import '../student/css/Graduation.css';
 import { useAtomValue } from 'jotai';
 import { memberAtom, tokenAtom } from '../../atoms';
@@ -16,10 +15,10 @@ import axios from 'axios';
 const AllmyGrades = () => {
     const token = useAtomValue(tokenAtom);
     const member = useAtomValue(memberAtom);
-    const [grades, setGrades] = useState([]);
+    // const [grades, setGrades] = useState([]);
+    const [lecList, setLecList] = useState([]);
+    const [courYear, setCourYear] = useState(1);
     const [semester, setSemester] = useState(2);
-    const [gradeList, setGradeList] = useState([]);
-    const [year, setYear] = useState(1);
 
     // const finSem = (finSem) => {
     //     const grade = Math.floor((finSem - 1) / 2 + 1);
@@ -28,25 +27,27 @@ const AllmyGrades = () => {
 
     useEffect(() => {
         if (token.access_token === '') return
-
-        console.log(member);
-        sendRequest(year,semester);
+        sendRequest(courYear,semester);
     }, [token])
-
-    const sendRequest = (year, semester) => {
-        axios.get(`${url}/allGrades?stdNo=${member.id}&year=${year}&semester=${semester}`, { headers: { Authorization: JSON.stringify(token) } })
-            .then(res => {
-                console.log(res.data);
-                setGradeList([...res.data.gradeList]);
-            });
-    };
 
     const checkGrade = (event) => {
         const newSemester = event.target.value;
         setSemester(newSemester);
 
-        sendRequest(year,semester);
+        sendRequest(courYear,newSemester);
     }
+
+    const sendRequest = (courYear, semester) => {
+        const gradeUrl = `${url}/allGrades?stdNo=${member.id}&courYear=${courYear}&semester=${semester}`;
+        console.log(gradeUrl)
+        axios.get(gradeUrl, { headers: { Authorization: JSON.stringify(token) } })
+            .then(res => {
+                console.log(res.data);
+                setLecList([...res.data.lecList]);
+            });
+    };
+
+
 
     return (
         <Grid item xs={12}>
@@ -73,8 +74,8 @@ const AllmyGrades = () => {
                             <div style={{ display: "flex", alignItems: 'center' }}>
                                 <FormControl sx={{ m: 1, minWidth: 150, float: "left", margin: "0 auto", marginRight: 4}} size="small">
                                     <Select id="demo-select-small"
-                                        value={year}
-                                        onChange={(event) => setYear(event.target.value)}
+                                        value={courYear}
+                                        onChange={(event) => setCourYear(event.target.value)}
                                     >
                                         <MenuItem value={1}>1학년</MenuItem>
                                         <MenuItem value={2}>2학년</MenuItem>
@@ -95,16 +96,6 @@ const AllmyGrades = () => {
                                     </Select>
                                 </FormControl>
                             </div>
-                            {/* <div className="col-3" style={{display:"flex", alignItems:'center', justifyContent:'center'}}>
-                                <span className="selText">구분</span>
-                                <Input type="select" className="selBox" style={{textAlign:'right'}}>
-                                    <option value="">전공</option>
-                                    <option value="APP">교양</option>
-                                </Input>
-                            </div> */}
-                            {/* <div className="col-4" style={{display:"flex", alignItems:'center', justifyContent:'center'}}>
-                                <Button variant="contained" size="medium" sx={{ margin: "0 auto", backgroundColor: "#1f3468" }}>조회</Button>
-                            </div> */}
                         </div>
 
                         <div className="miniCard">
@@ -113,25 +104,16 @@ const AllmyGrades = () => {
                                 <div className="col-4 type">등급</div>
                                 <div className="col-4 type">구분</div>
                             </div>
-                            {gradeList.map(grade => (
+                            <div style={{height:'500px', overflowY:'scroll', marginBottom:'20px'}}>
+                            {lecList.map(lec => (
                                 <div className="list">
-                                    <div className="col-4 con">{grade.subjectName}</div>
-                                    <div className="col-4 con">{grade.grade}</div>
+                                    <div className="col-4 con">{lec.subName}</div>
+                                    <div className="col-4 con">{lec.grade}</div>
                                     <div className="col-4 con">전공</div>
                                 </div>
                             ))}
+                            </div>    
 
-
-                            {/* <div className="list">
-                                <div className="col-4 con">게임프로그래밍실습</div>
-                                <div className="col-4 con">A+</div>
-                                <div className="col-4 con">전공</div>
-                            </div>
-                            <div className="list">
-                                <div className="col-4 con">게임프로그래밍실습</div>
-                                <div className="col-4 con">A+</div>
-                                <div className="col-4 con">전공</div>
-                            </div> */}
                         </div>
 
                     </Grid>
