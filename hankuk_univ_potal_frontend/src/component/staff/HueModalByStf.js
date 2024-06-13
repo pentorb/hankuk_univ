8460cf2ceccd55c459497d5fb991a75f5008582b
Hyun <83358018+hyunmne@users.noreply.github.com>
2024-06-13 18:49:31 +0900

    design: 휴복학 화면 .. 교직원 ..  모달까쥐~

---
+++ hankuk_univ_potal_frontend/src/component/staff/HueModalByStf.js
@@ -0,0 +1,165 @@
+import { Divider, Radio } from '@mui/material';
+import axios from 'axios';
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import { url } from '../../config/config';
+import { useNavigate } from 'react-router';
+import { tokenAtom } from '../../atoms';
+import { useAtomValue } from 'jotai';
+import Swal from "sweetalert2";
+
+const text = {
+    display: "flex",
+    margin: "10px",
+    fontWeight: "bold",
+    fontSize: "larger",
+    textAlign: "left"
+};
+
+const typeMap = {
+    p: '출산, 임신 휴학',
+    k: '육아 휴학',
+    o: '일반 휴학',
+    m: '군 휴학',
+    s: '창업 휴학',
+    i: '질병 휴학'
+};
+
+const HueModalByStf = ({ isOpen, toggle, data }) => {
+    const token = useAtomValue(tokenAtom);
+    const [selectedValue, setSelectedValue] = React.useState('APP');
+    const [values, setValues] = React.useState({
+        rejResult: '',
+        status: 'APP'
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+        setValues({ ...values, status: event.target.value });
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    const alert = (e) => {
+        e.preventDefault();
+        Swal.fire({
+            title: "휴학 상태 변경이 완료되었습니다.",
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "취소",
+            confirmButtonText: "확인"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                toggle();
+            }
+        });
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("stdNo", data.stdNo);
+        formData.append("type", data.type);
+        formData.append("hueSem", data.hueSem);
+        formData.append("files", data.files);
+        formData.append("reason", data.reason);
+        formData.append("status", values.status);
+        formData.append("rejResult", values.rejResult);
+        formData.append("hueNo", data.hueNo);
+
+        axios.post(`${url}/hueStatus`, formData, { headers: { Authorization: JSON.stringify(token) } })
+            .then(res => {
+                console.log(res.data);
+                navigate('/staff/confirmHuehak');
+                // toggle();
+                alert(e);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    return (
+        <div style={{ position: 'absolute' }}>
+            <Modal isOpen={isOpen} toggle={toggle} style={{ width: '630px' }}>
+                <ModalHeader toggle={toggle}>{data.hueNo}번 휴학 승인</ModalHeader>
+                <ModalBody>
+                    <form onSubmit={submit}>
+                        <div className="col-12" style={{ display: 'flex', justifyContent: 'space-around' }}>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>번호</div>
+                                <div>{data.hueNo}</div>
+                            </div>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>학번</div>
+                                <div>{data.stdNo}</div>
+                            </div>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>이름</div>
+                                <div>{data.stdNm}</div>
+                            </div>
+                        </div>
+                        <div className="col-12" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>단과</div>
+                                <div>{data.colNm}</div>
+                            </div>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>학과</div>
+                                <div>{data.colNm}</div>
+                            </div>
+                            <div className="col-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                                <div style={text}>유형</div>
+                                <div>{typeMap[data.type] || data.type}</div>
+                            </div>
+                        </div>
+                        <Divider sx={{ margin: 3, borderColor: 'black' }} />
+                        <div className="col-12" style={{ marginLeft: '10px' }}>
+                            <div style={text}>휴학 사유</div>
+                            <div style={{ margin: '10px 30px 20px 10px', padding: '10px', height: '110px', border: '1px solid lightgrey', borderRadius: '10px' }}>{data.reason}</div>
+                        </div>
+                        {data.files && (
+                            <div className="col-12" style={{ marginLeft: '10px' }}>
+                                <div style={text}>첨부파일</div>
+                                <div style={{ margin: '10px 30px 20px 10px' }}>{data.files}</div>
+                            </div>
+                        )}
+                        <div className="col-12" style={{ marginLeft: '10px', display: 'flex', justifyContent: 'space-between' }}>
+                            <div style={text}>상태</div>
+                            <div style={{ margin: '10px 30px 20px 10px', display: 'flex', alignItems: 'center' }}>
+                                <input type="radio" value="APP" checked={selectedValue === 'APP'}
+                                    onChange={handleChange} name="status" />&nbsp; 승인 &nbsp; &nbsp;
+                                <input type="radio" value="REJ" checked={selectedValue === 'REJ'}
+                                    onChange={handleChange} name="status" />&nbsp; 반려
+                            </div>
+                        </div>
+                        {selectedValue === 'REJ' && (
+                            <div className="col-12" style={{ marginLeft: '10px' }}>
+                                <div style={text}>반려 사유</div>
+                                <div style={{ margin: '10px 30px 20px 10px' }}>
+                                    <Input id="rejResult" name="rejResult"
+                                        placeholder='반려사유를 명확하게 입력해주세요' type="textarea"
+                                        style={{ padding: '10px', height: '110px' }} onChange={handleInputChange} />
+                                </div>
+                            </div>
+                        )}
+                        <ModalFooter>
+                            <Button color="secondary" onClick={toggle}>취소</Button>
+                            <Button color="primary" type="submit">저장</Button>
+                        </ModalFooter>
+                    </form>
+                </ModalBody>
+            </Modal>
+        </div>
+    );
+};
+
+export default HueModalByStf;
--- hankuk_univ_potal_frontend/src/component/staff/StaffHuehak.js
+++ hankuk_univ_potal_frontend/src/component/staff/StaffHuehak.js
@@ -1,12 +1,19 @@
 import Grid from '@mui/material/Grid';
-import { Paper, Typography, Select, MenuItem, FormControl } from '@mui/material';
+import { Paper, Typography, Stack, Pagination } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 import StopRoundedIcon from '@mui/icons-material/StopRounded';
-import { Table, Input, Button } from 'reactstrap';
+import { Table, Input } from 'reactstrap';
+import { useEffect, useState } from 'react';
+import { url } from '../../config/config';
+import { useAtomValue } from 'jotai/react';
+import { tokenAtom } from '../../atoms';
+import axios from 'axios';
+import '../staff/css/HuehakByStf.css';
+import HueModalByStf from './HueModalByStf';
 
 const typeMap = {
     p: '출산, 임신 휴학',
@@ -23,27 +30,90 @@ const statusMap = {
     APP: '승인'
 };
 
+function parseSemester(data) {
+    const year = Math.floor(data / 100);
+    const semester = data % 100;
+    return `${year}년도 ${semester}학기`;
+}
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear().toString().slice(2); // 년도에서 마지막 두 자리
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월 (0부터 시작하므로 1을 더함)
+    const day = date.getDate().toString().padStart(2, '0'); // 일
+
+    return `${year}.${month}.${day}`;
+}
+
+function getStatusStyle(status) {
+    switch (status) {
+        case 'REQ':
+            return { color: 'blue' };
+        case 'APP':
+            return { color: 'green' };
+        case 'REJ':
+            return { color: 'red' };
+        default:
+            return {};
+    }
+}
+
 const StaffHuehak = () => {
-    // const [huebok, setHuebok] = useState([]);
-    // const token = useAtomValue(tokenAtom);
-    // const member = useAtomValue(memberAtom);
+    const [allhbs, setAllhbs] = useState([]);
+    const token = useAtomValue(tokenAtom);
+    const [type, setType] = useState('');
+    const [pageInfo, setPageInfo] = useState({
+        curPage: 1,
+        startPage: 1,
+        endPage: 1,
+        allPage: 1
+    });
+    const [pageBtn, setPageBtn] = useState([]);
+    const [modal, setModal] = useState(false);
+    const [selectedHb, setSelectedHb] = useState(null);
+    const toggle = () => setModal(!modal);
 
-    // useEffect(() => {
-    //     console.log(token);
-    //     if(token.access_token==='') return
+    const handleChangeType = (e) => {
+        const selectedType = e.target.value;
+        setType(selectedType); // 선택한 값으로 state 업데이트
+    };
 
-    //     axios.get(`${url}/hueListByStdNo`, { headers: { Authorization: JSON.stringify(token) } })
-    //         .then(res => {
-    //             console.log(res.data);
-    //             setHuebok([...res.data])
-    //         });
-    // }, [token]);
+    const handleRowClick = (hb) => {
+        setSelectedHb(hb);
+        setModal(true);
+    };
 
-    
+    useEffect(() => {
+        if (token.access_token === '') return;
+        fetchData(1);
+    }, [token, type]);
+
+    const fetchData = (page) => {
+        const listUrl = `${url}/allHBList?page=${page}&type=${type}`;
+        axios.get(listUrl, { headers: { Authorization: JSON.stringify(token) } })
+            .then(res => {
+                setAllhbs([...res.data.allhbList]);
+                setPageInfo({ ...res.data.pageInfo }); 
+                // 페이지 버튼 설정
+                let pageButtons = [];
+                for (let i = res.data.pageInfo.startPage; i <= res.data.pageInfo.endPage; i++) {
+                    pageButtons.push(i);
+                }
+                setPageBtn([...pageButtons]);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    };
+
+    const handlePageChange = (page) => {
+        fetchData(page); // 페이지 변경 시 데이터 다시 가져오기
+    };
+
     return (
         <Grid item xs={12}>
             <Typography ml={18} mt={10} mb={3} variant="h4" color="#444444" gutterBottom><b>휴복학 신청 관리</b></Typography>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: "auto", overflow: "hidden", width: 1400, margin: "0 auto", borderRadius: 5 }}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', minHeight: '800px', height: "auto", overflow: "hidden", width: 1400, margin: "0 auto", borderRadius: 5 }}>
                 <div id="breadCrumb" style={{ margin: '24px 40px 32px' }}>
                     <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon fontSize="small" />}>
                         <Link underline="none" color="inherit" href="/student">
@@ -65,18 +135,12 @@ const StaffHuehak = () => {
                             <StopRoundedIcon fontSize='small' /> &nbsp;&nbsp;
                             <span style={{ fontSize: 'x-large' }}><b>휴학 신청 현황</b></span>
                         </div>
-                        <div style={{ padding: '0px 50px 30px', textAlign: 'center', fontSize: 'larger' }}>
+                        <div style={{ padding: '0px 50px 10px', textAlign: 'center', fontSize: 'larger' }}>
                             <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                                <Input type="select" className="selBox">
-                                    <option>처리 현황</option>
-                                    <option value="REQ">신청</option>
-                                    <option value="REJ">반려</option>
-                                    <option value="APP">완료</option>
-                                </Input>&nbsp;&nbsp;&nbsp;
-                                <Input type="select" className="selBox" id="type" name="type">
+                                <Input type="select" className="selBox" id="type" name="type" onChange={handleChangeType}>
                                     <option>구분</option>
                                     <option value="o">일반 휴학</option>
-                                    {/* {member.gender !== 'F' ? (<><option value="m">군 휴학</option></>) : ''} */}
+                                    <option value="m">군 휴학</option>
                                     <option value="p">출산, 임신 휴학</option>
                                     <option value="s">창업 휴학</option>
                                     <option value="i">질병 휴학</option>
@@ -86,37 +150,47 @@ const StaffHuehak = () => {
                             <Table className="table" bordered>
                                 <thead>
                                     <tr>
-                                        <th>휴학 번호</th>
-                                        <th>휴학 유형</th>
-                                        <th>휴학 신청 일자</th>
+                                        <th>신청 번호</th>
+                                        <th>학번</th>
+                                        <th>이름</th>
+                                        <th>소속</th>
+                                        <th>학과</th>
+                                        <th>유형</th>
                                         <th>휴학 학기</th>
-                                        <th>처리 상태</th>
+                                        <th>일자</th>
+                                        <th>상태</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {/* <tr>
-                                        <td scope="row">{hue.hueNo}</td>
-                                        <td>{typeMap[hue.type] || hue.type}</td>
-                                        <td>{hue.appDt}</td>
-                                        <td>{hue.hueSem}</td>
-                                        <td>{statusMap[hue.status] || hue.status}</td>
-                                    </tr> */}
-
-                                    {/* {huebok.filter(hue => hue.sect === 'H').map(hue => (
-                                        <tr key={hue.hueNo}>
-                                            <td scope="row">{hue.hueNo}</td>
-                                            <td>{typeMap[hue.type] || hue.type}</td>
-                                            <td>{hue.appDt}</td>
-                                            <td>{hue.hueSem}</td>
-                                            <td>{statusMap[hue.status] || hue.status}</td>
+                                    {allhbs.filter(hb => hb.sect === 'H' || hb.status === 'REQ').map(hb => (
+                                        <tr key={hb.hueNo} onClick={() => handleRowClick(hb)}>
+                                            <td scope="row">{hb.hueNo}</td>
+                                            <td>{hb.stdNo}</td>
+                                            <td>{hb.stdNm}</td>
+                                            <td>{hb.colNm}</td>
+                                            <td>{hb.majNm}</td>
+                                            <td>{typeMap[hb.type] || hb.type}</td>
+                                            <td>{parseSemester(hb.hueSem)}</td>
+                                            <td>{formatDate(hb.appDt)}</td>
+                                            <td style={getStatusStyle(hb.status)}>
+                                                {/* <Button style={{backgroundColor: "#1F3468"}}>  */}
+                                                <span onClick={<HueModalByStf />}>
+                                                    {statusMap[hb.status] || hb.status}
+                                                </span>
+                                                {/* </Button> */}
+                                            </td>
                                         </tr>
-                                    ))} */}
+                                    ))}
                                 </tbody>
                             </Table>
                         </div>
-
+                        <Stack spacing={2} alignItems="center" sx={{ marginBottom: 1 }}>
+                            <Pagination count={pageInfo.allPage} page={pageInfo.curPage} onChange={(e, page) => handlePageChange(page)} />
+                        </Stack>
+                        {selectedHb && (
+                            <HueModalByStf isOpen={modal} toggle={toggle} data={selectedHb} />
+                        )}
                     </Grid>
-
                     <Grid item xs={1}></Grid>
                 </Grid>
             </Paper>
