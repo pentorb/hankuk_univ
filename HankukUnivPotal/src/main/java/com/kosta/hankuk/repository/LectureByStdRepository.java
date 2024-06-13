6065c6f451d3baf0ec7012453a02e8a91958c424
Youngwoo Choi <allnstar@naver.com>
2024-06-13 12:04:35 +0900

    remove: 수강학년으로 조회하지 않는 메소드 삭제

--- HankukUnivPotal/src/main/java/com/kosta/hankuk/repository/LectureByStdRepository.java
+++ HankukUnivPotal/src/main/java/com/kosta/hankuk/repository/LectureByStdRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kosta.hankuk.entity.LectureByStd;
 
 public interface LectureByStdRepository extends JpaRepository<LectureByStd, Integer>{
-	List<LectureByStd> findByStudent_stdNoAndLecture_yearAndLecture_semester(String stdNo, Integer year, Integer semester);
 	LectureByStd findByStudent_stdNoAndLecture_lecNo(String stdNo, String lecNo);
 	
 	// 학생 개인의 수강 리스트 조회 (학년별, 학기별)
