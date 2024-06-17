3bc69630c21dceebd22959d4883429862d65dd6f
hyunmne <83358018+hyunmne@users.noreply.github.com>
2024-06-17 23:42:59 +0900

    feat: 복학 상태변경

--- hankuk_univ_potal_frontend/src/component/student/BokhakInsert.js
+++ hankuk_univ_potal_frontend/src/component/student/BokhakInsert.js
@@ -15,6 +15,7 @@ import { url } from '../../config/config';
 import { memberAtom, tokenAtom } from '../../atoms';
 import React, { useEffect, useState } from 'react';
 import BokInfo from '../student/BokInfo';
+import BokModalByStd from './BokModalByStd';
 
 const typeMap = {
     p: '출산, 임신 휴학',
@@ -57,28 +58,25 @@ const BokhakInsert = () => {
     const [status, setStatus] = useState('');
     const member = useAtomValue(memberAtom);
     const [type, setType] = useState('');
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+    const [selectedHue, setSelectedHue] = useState(null);
+    const [pageBtn, setPageBtn] = useState([]);
     const [pageInfo, setPageInfo] = useState({
         curPage: 1,
         startPage: 1,
         endPage: 1,
         allPage: 1
     });
-    const [pageBtn, setPageBtn] = useState([]);
     const handlePageChange = (page) => {
         fetchData(page); // 페이지 변경 시 데이터 다시 가져오기
     };
 
-    const handleTypeChange = (e) => {
-        const selectData = e.target.value;
-        setType(selectData);
-    }
-
-    const alert = (e) => {
-        e.preventDefault();
+    const trClick = (hb) => {
+        setSelectedHue(hb);
         Swal.fire({
-            title: "복학 신청을 완료하시겠습니까?",
-            text: "한 번 제출된 복학신청은 철회할 수 없습니다.",
-            icon: 'warning',
+            title: "복학 신청하시겠습니까?",
+            icon: 'info',
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
@@ -86,63 +84,10 @@ const BokhakInsert = () => {
             confirmButtonText: "확인"
         }).then((result) => {
             if (result.isConfirmed) {
-                submit(e);
+                toggle();
             }
         });
-    }
 
-    const [formValues, setFormValues] = React.useState({
-        stdNo: '',
-        type: '',
-        year: '',
-        sem: '',
-        files: '',
-        reason: ''
-    });
-
-    const dataChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });
-    }
-
-
-    const submit = (e) => {
-        const formData = new FormData();
-        formData.append("stdNo", member.id);
-        formData.append("type", formValues.type);
-        formData.append("hueSem", formValues.year + formValues.sem);
-        formData.append("files", formValues.files);
-        formData.append("reason", formValues.reason);
-        formData.append("sect", 'B');
-
-        axios.post('http://localhost:8090/hueInsert', formData)
-            .then(res => {
-                console.log(res.data);
-                navigate('/student/resSemester')
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    const [selectedHue, setSelectedHue] = useState({
-        hueNo: '',
-        type: '',
-        appDt: '',
-        hueSem: '',
-        status: ''
-    });
-
-    const trClick = (hb) => {
-        // axios.get(`${url}/hueDetail?hueNo=${hb.hueNo}`, { headers: { Authorization: JSON.stringify(token) } })
-        //     .then(res => {
-        //         const huehak = res.data; // 서버에서 반환한 데이터 객체
-        //         // setHueDetail(huehak); // hueDetail 상태 업데이트
-        //     })
-        //     .catch(error => {
-        //         console.error('Error fetching data:', error);
-        //     });
-        setSelectedHue(hb);
     };
 
     useEffect(() => {
@@ -169,6 +114,12 @@ const BokhakInsert = () => {
                 console.error('Error fetching data:', error);
             });
     };
+
+    // 이수 학기로 학년 구하기 
+    const finSem = (finSem) => {
+        const grade = Math.floor((finSem-1) / 2 + 1);
+        return `${grade}학년`;
+    }
 
     return (
         <Grid item xs={12}>
@@ -203,14 +154,49 @@ const BokhakInsert = () => {
 
                         <div className="categori">
                             <StopRoundedIcon fontSize='small' /> &nbsp;&nbsp;
+                            <span style={{ fontSize: 'x-large' }}><b>학적기초정보</b></span>
+                        </div>
+                        <div style={{ padding: '10px 50px 30px', textAlign: 'center', fontSize: 'larger' }}>
+                            <Table className="table" bordered>
+                                <thead>
+                                    <tr>
+                                        <th>학번</th>
+                                        <th>이름</th>
+                                        <th>학부(과)</th>
+                                        <th>학년</th>
+                                        <th>학적 상태</th>
+                                        <th>학생 연락처</th>
+                                        <th>지도교수</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td scope="row">{member.id}</td>
+                                        <td>{member.name}</td>
+                                        <td>{member.majName}</td>
+                                        <td>{finSem(member.finSem)}</td>
+                                        <td>{statusMap[member.status] || member.status}</td>
+                                        <td>{member.tel}</td>
+                                        <td>{member.profName}</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </div>
+
+
+                        <div className="categori" style={{justifyContent:'space-between'}}>
+                            <div style={{display:'flex', alignItems:'center'}}>
+                            <StopRoundedIcon fontSize='small' /> &nbsp;&nbsp;
                             <span style={{ fontSize: 'x-large' }}><b>휴학 내역</b></span>
+                            </div>
+                            <span style={{marginRight:'41px', color:'lightgrey'}}>해당 휴학 내역을 선택하고 복학 신청을 진행해주세요.</span>
                         </div>
                         <div style={{ padding: '0px 50px 0px', textAlign: 'center', fontSize: 'larger' }}>
                             {huehak.length === 0 ? (
                                 <div className="noneData">조회 내역이 없습니다.</div>
                             ) : (
                                 <>
-                                    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'flex-end', marginRight:'0px' }}>
                                         <Input type="select" className="selBox" name="status">
                                             <option>처리 현황</option>
                                             <option value="REQ">신청</option>
@@ -261,110 +247,12 @@ const BokhakInsert = () => {
                                     <Stack spacing={2} alignItems="center" sx={{ marginBottom: 1 }}>
                                         <Pagination count={pageInfo.allPage} page={pageInfo.curPage} onChange={(e, page) => handlePageChange(page)} />
                                     </Stack>
+                                    {selectedHue && (
+                                        <BokModalByStd isOpen={modal} toggle={toggle} data={selectedHue} />
+                                    )}
                                 </>
                             )}
                         </div>
-                        <Divider sx={{margin:3}} />
-                        <div className="categori">
-                            <StopRoundedIcon fontSize='small' /> &nbsp;&nbsp;
-                            <span style={{ fontSize: 'x-large' }}><b>복학 신청</b></span>
-                        </div>
-
-                        <form onSubmit={submit}>
-                            <div style={{ display: 'flex', padding: '10px' }}>
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">복학 구분</div>
-                                    <div className="col-6">
-                                        <Input type="select" id="type" name="type"
-                                            onChange={dataChange} value={selectedHue.type}>
-                                            <option>---선택하세요---</option>
-                                            <option value="o">일반 복학</option>
-                                            {member.gender !== 'F' ?
-                                                (<>
-                                                    <option value="m">군 복학</option>
-                                                </>
-                                                ) : (<></>)
-                                            }
-                                            <option value="p">출산, 임신 복학</option>
-                                            <option value="s">창업 복학</option>
-                                            <option value="i">질병 복학</option>
-                                            <option value="k">육아 복학</option>
-                                        </Input>
-                                    </div>
-                                </div>
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">신청 일자</div>
-                                    <div className="col-6">
-                                        <Input
-                                            id="exampleDate"
-                                            name="date"
-                                            placeholder="date placeholder"
-                                            type="date"
-                                            onChange={dataChange}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">휴학 번호</div>
-                                    <div className="col-6">
-                                    <Input type="select" id="type" name="type" onChange={dataChange} value={selectedHue.hueNo}>
-                                        {huehak.map(hue => (
-                                            <option key={hue.hueNo} value={hue.hueNo}>
-                                                {hue.hueNo}
-                                            </option>
-                                        ))}
-                                    </Input>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div style={{ display: 'flex', padding: '10px' }}>
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">복학 년도</div>
-                                    <div className="col-6">
-                                        <Input type="text" id="year" name="year"
-                                            placeholder='2024'
-                                            onChange={dataChange}
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">복학 학년</div>
-                                    <div className="col-6">
-                                        <Input type="select" id="sem" name="sem"
-                                            onChange={dataChange}>
-
-                                            <option>학년 선택</option>
-                                            <option value="01">1학년</option>
-                                            <option value="02">2학년</option>
-                                            <option value="02">3학년</option>
-                                            <option value="02">4학년</option>
-                                        </Input>
-                                    </div>
-                                </div>
-
-
-                                <div className="col-4" style={{ display: 'flex', alignItems: 'center' }}>
-                                    <div className="col-4 title">복학 학기</div>
-                                    <div className="col-6">
-                                        <Input type="select" id="sem" name="sem"
-                                            onChange={dataChange}>
-                                            <option>학기 선택</option>
-                                            <option value="01">1학기</option>
-                                            <option value="02">2학기</option>
-                                        </Input>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div className="col-12" style={{ padding: '50px 30px 0px', display: 'flex', justifyContent: 'center' }}>
-                                <Button style={{ backgroundColor: '#1F3468' }} onClick={alert}>복학 신청</Button>
-                            </div>
-                        </form>
-
-                        
                     </Grid>
                     <Grid item xs={1}></Grid>
                 </Grid>
