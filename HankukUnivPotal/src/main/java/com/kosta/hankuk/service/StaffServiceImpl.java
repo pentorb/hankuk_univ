de1f0f08b87b515f4e34c58b546460f56d78c474
hyunmne <83358018+hyunmne@users.noreply.github.com>
2024-06-24 23:05:26 +0900

    feat: 학사 공지 게시판 글 작성

--- HankukUnivPotal/src/main/java/com/kosta/hankuk/service/StaffServiceImpl.java
+++ HankukUnivPotal/src/main/java/com/kosta/hankuk/service/StaffServiceImpl.java
@@ -821,6 +821,7 @@ public class StaffServiceImpl implements StaffService {
 	@Override // 게시물 작성
 	public void noticeWrite(NoticeBoardDto nbrdDto) {
 		NoticeBoard nbrd = nbrdDto.toNoticeBoard();
+		nbrd.setViewCount(0);
 		nbres.save(nbrd);
 	}
 
