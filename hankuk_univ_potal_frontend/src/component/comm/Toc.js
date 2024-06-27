325bd52f96ce7059819d93fc54c0967179108b1b
Hyun <83358018+hyunmne@users.noreply.github.com>
2024-06-27 10:35:44 +0900

    fix: Toc 교수 -> 수강신청 보이지 않게 수정

--- hankuk_univ_potal_frontend/src/component/comm/Toc.js
+++ hankuk_univ_potal_frontend/src/component/comm/Toc.js
@@ -7,30 +7,52 @@ const Toc = () => {
     const navigate = useNavigate();
 
     const goPotal = () => {
-        if (member.id.substring(0, 1) === "P") {
+        if (member?.id?.substring(0, 1) === "P") {
             navigate("/professor");
-        } else if (member.id.substring(0, 1) === "S") {
+        } else if (member?.id?.substring(0, 1) === "S") {
             navigate("/staff");
-        } else if (member.id.substring(0, 1) === "2") {
+        } else if (member?.id?.substring(0, 1) === "2") {
             navigate("/student");
         }
     }
+
     return (
         <div className="btn-group-vertical">
-            <Link to="/course-registration" className="toc_bnt">
-                <img src="\images\university.png" style={{ width: "50px", height: "50px" }} alt="" />
-                <div style={{ paddingTop: "10px" }}>수강신청</div>
-            </Link>
-            <div onClick={goPotal} className="toc_bnt" style={{cursor:"pointer"}}>
-                <img src="\images\student.png" style={{ width: "50px", height: "50px" }} alt="" />
-                <div style={{ paddingTop: "10px" }}>마이 포탈</div>
-            </div>
-            <Link to="/noticeBoard" className="toc_bnt">
-                <img src="\images\university2.png" style={{ width: "50px", height: "50px" }} alt="" />
-                <div style={{ paddingTop: "10px" }}>통합 게시판</div>
-            </Link>
+            {!member?.id ? (
+                <>
+                    <Link to="/course-registration" className="toc_bnt">
+                        <img src="\images\university.png" style={{ width: "50px", height: "50px" }} alt="" />
+                        <div style={{ paddingTop: "10px" }}>수강신청</div>
+                    </Link>
+                    <div onClick={goPotal} className="toc_bnt" style={{ cursor: "pointer" }}>
+                        <img src="\images\student.png" style={{ width: "50px", height: "50px" }} alt="" />
+                        <div style={{ paddingTop: "10px" }}>마이 포탈</div>
+                    </div>
+                    <Link to="/noticeBoard" className="toc_bnt">
+                        <img src="\images\university2.png" style={{ width: "50px", height: "50px" }} alt="" />
+                        <div style={{ paddingTop: "10px" }}>통합 게시판</div>
+                    </Link>
+                </>
+            ) : (
+                <>
+                    {member.id.substring(0, 1) !== "P" && (
+                        <Link to="/course-registration" className="toc_bnt">
+                            <img src="\images\university.png" style={{ width: "50px", height: "50px" }} alt="" />
+                            <div style={{ paddingTop: "10px" }}>수강신청</div>
+                        </Link>
+                    )}
+                    <div onClick={goPotal} className="toc_bnt" style={{ cursor: "pointer" }}>
+                        <img src="\images\student.png" style={{ width: "50px", height: "50px" }} alt="" />
+                        <div style={{ paddingTop: "10px" }}>마이 포탈</div>
+                    </div>
+                    <Link to="/noticeBoard" className="toc_bnt">
+                        <img src="\images\university2.png" style={{ width: "50px", height: "50px" }} alt="" />
+                        <div style={{ paddingTop: "10px" }}>통합 게시판</div>
+                    </Link>
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default Toc;
